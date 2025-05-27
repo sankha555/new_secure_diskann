2f660ddfe82b0880b741b13cf43cd9eb4ce3a3a8
sankha555 <f20190029@pilani.bits-pilani.ac.in>
2025-05-27 08:52:41 +0000

    added a mini setup script

---
+++ data/gcs_download.py
@@ -0,0 +1,26 @@
+from google.cloud import storage
+import os
+
+def download_public_bucket(bucket_name, destination_dir):
+    client = storage.Client.create_anonymous_client()
+    bucket = client.bucket(bucket_name)
+    blobs = bucket.list_blobs()
+
+    for blob in blobs:
+        dest_file_path = os.path.join(destination_dir, blob.name)
+        os.makedirs(os.path.dirname(dest_file_path), exist_ok=True)
+
+        # Check if file already exists locally
+        if not os.path.exists(dest_file_path):
+            if not "laion" in dest_file_path:
+                continue
+            print(f"Downloading {blob.name}...")
+            blob.download_to_filename(dest_file_path)
+        else:
+            print(f"{blob.name} already exists, skipping download.")
+
+# Usage
+bucket_name = 'compass_osdi'
+destination_dir = './data/'
+
+download_public_bucket(bucket_name, destination_dir)
